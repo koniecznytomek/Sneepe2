32580637ce645526a2db85682b78c5122f341d1d
Tomek Konieczny <tomkowaty@gmail.com>
2021-10-24 13:33:38 +0200

    update

--- public/electron.js
+++ public/electron.js
@@ -1,47 +1,26 @@
-const path = require('path');
-
-const { app, BrowserWindow } = require('electron');
-const isDev = require('electron-is-dev');
-
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const path = require("path");
+const isDev = require("electron-is-dev");
+let mainWindow;
 function createWindow() {
-    // Create the browser window.
-    const win = new BrowserWindow({
-        width: 800,
-        height: 600,
-        webPreferences: {
-            nodeIntegration: true,
-        },
-    });
-
-    // and load the index.html of the app.
-    // win.loadFile("index.html");
-    win.loadURL(
+    mainWindow = new BrowserWindow({ width: 900, height: 680 });
+    mainWindow.loadURL(
         isDev
-            ? 'http://localhost:3000'
-            : `file://${path.join(__dirname, '../build/index.html')}`
+            ? "http://localhost:3000"
+            : `file://${path.join(__dirname, "../build/index.html")}`
     );
-    // Open the DevTools.
-    if (isDev) {
-        win.webContents.openDevTools({ mode: 'detach' });
-    }
+    mainWindow.on("closed", () => (mainWindow = null));
 }
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-app.whenReady().then(createWindow);
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
+app.on("ready", createWindow);
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
+app.on("activate", () => {
+    if (mainWindow === null) {
         createWindow();
     }
 });
