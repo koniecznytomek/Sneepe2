5beaefc43619785fb6905c2c96ae48e8a4dd3558
Tomek Konieczny <tomkowaty@gmail.com>
2021-10-24 18:12:52 +0200

    update

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -5,7 +5,15 @@ import { Container } from './style';
 
 const Login: React.FunctionComponent = () => {
     const redirect = 'http://localhost:3000/auth';
-    const client = `${process.env.REACT_APP_CLIENT_ID}`
+    const client = `${process.env.REACT_APP_CLIENT_ID}`;
+    // const secret = `${process.env.REACT_APP_CLIENT_SECRET}`;
+
+    const value = new URLSearchParams(window.location.search).get('code');
+
+    if (value) {
+        console.log(value);
+
+    }
 
     return (
         <Container>
